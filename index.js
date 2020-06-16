1b37d9d4c207afefeb0a8d0859a4d8db28985107
like <q747367352@gmail.com>
2020-06-16 16:24:20 +0800

    init

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 // Complete the following procedure, the 'name' variable in all letters to uppercase, output: ' 'HELLO' '.
 var name = 'hello';
 // TODO write your code here...
-
+name = name.toUpperCase();
+console.log(name);
 
 // Complete the following procedure, capitalizing all the words in the variable sentence and printing: 'Good Afternoon, Mr Mike.'
 var sentence = 'good afternoon, mr mike.';
 //TODO write your code here...
-
-
+sentence = sentence.toLowerCase().replace(/( |^)[a-z]/g, (L) => L.toUpperCase());
+console.log(sentence);
 // Complete the following procedure to extract the amount of money variable, output: 20.
 var money = '￥20';
 //TODO write your code here...
+money = money.replace('￥', '');
+console.log(money);
